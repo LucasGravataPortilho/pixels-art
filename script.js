5aa99e3c2a3cf3f171a37a78aab3b8c808eef54d
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-31 17:13:12 -0300

    iniciando requisito 4 e adicionando gerador de cores aleatorio

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ colorTask('black');
 colorTask('blue');
 colorTask('red');
 colorTask('green');
+
+function generateColor() {
+    const letras = '0123456789ABCDEF';
+    let colorzao = '#'
+
+    for (let x = 0; x < 6; x += 1) {
+        colorzao += letras[Math.floor(Math.random() * 16)];
+    }
+
+    return colorzao;
+}
+
+console.log(generateColor());
+
+// function randomColor() {
+//     let cores = document.querySelectorAll('.color');
+//     let botaoRandom = document.querySelector('#button-random-color');
+
+//     botaoRandom.addEventListener('click', function() {
+//         for(let i = 1; i < cores.length; i += 1) {
+//             cores[i].style.backgroundColor =
+//         }
+//     })
+    
+// }
